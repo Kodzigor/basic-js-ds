3cc03349f9f3d5fd35d21c5823e1d16e4ee2caa0
Igor Kodzhebash <igorkodzebash@gmail.com>
2023-04-17 18:25:03 +0300

    done queue

--- src/queue.js
+++ src/queue.js
@@ -13,24 +13,54 @@ const { NotImplementedError } = require('../extensions/index.js');
  * queue.dequeue(); // returns the top element from queue and deletes it, returns 1
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
+
+class ListNode {
+    constructor(x) {
+        this.value = x;
+        this.next = null;
+    }
+}
 class Queue {
+    constructor() {
+        this.head = null; // Points to the head of the queue
+        this.tail = null; // Points to the tail of the queue
+    }
 
-  getUnderlyingList() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+    // Adds a value to the end of the queue
+    enqueue(value) {
+        const newNode = new ListNode(value);
+        if (!this.head) {
+            // If the queue is empty, set both head and tail to the new node
+            this.head = newNode;
+            this.tail = newNode;
+        } else {
+            // If the queue is not empty, append the new node to the tail
+            this.tail.next = newNode;
+            this.tail = newNode;
+        }
+    }
 
-  enqueue(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+    // Retrieves and removes a value from the head of the queue
+    dequeue() {
+        if (!this.head) {
+            // If the queue is empty, return null
+            return null;
+        }
+        const dequeuedValue = this.head.value; // Get the value of the head node
+        this.head = this.head.next; // Move the head to the next node
+        if (!this.head) {
+            // If the queue becomes empty after dequeuing, set the tail to null as well
+            this.tail = null;
+        }
+        return dequeuedValue;
+    }
 
-  dequeue() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+    // Returns the underlying linked list
+    getUnderlyingList() {
+        return this.head;
+    }
 }
 
 module.exports = {
-  Queue
+    Queue,
 };
